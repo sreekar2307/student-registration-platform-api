0ee3fbcf297a5958de9bcca14e91f41040d35b32
sreekar2307 <bollamsreekhar@gmail.com>
2021-04-18 16:21:48 +0530

    improve validations on student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,9 +4,10 @@ class Student < ApplicationRecord
   has_many :student_courses_map, class_name: 'StudentsCourses'
   has_many :courses, through: :student_courses_map, dependent: :destroy, before_add: :courses_enrolled
   validates :degree, presence: { message: 'Student must be assigned to a degree' }
-  validates :name, presence: { message: 'Name of a student cannot be empty' }
+  validates :name, presence: { message: 'Name of a student cannot be empty' }, length: { minimum: 3 }
   validates :email, presence: { message: 'Email address of a student cannot be empty' },
-            uniqueness: { message: 'Email address of a student has to be unique' }
+            uniqueness: { message: 'Email address of a student has to be unique' },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :enrollment_no, presence: { message: 'Enrollment Number of a student cannot be empty' },
             uniqueness: { message: 'Enrollment Number of a student has to be unique' }
 
